364c3105dc4fa40e7bb06c6f2269fdcf14e3b8f1
theChandrayMurmu <thechandraymurmu@gmail.com>
2024-08-19 16:04:04 +0530

    add shadcn/ui component
    exploring the shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +29,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div>
+        <Button>Join Now </Button>
+      </div>
     </>
   )
 }
